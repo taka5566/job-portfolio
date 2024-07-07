6cee6dbc45d4816126174b4286ad10174cc23bf6
taka5566 <139129841+taka5566@users.noreply.github.com>
2024-07-08 02:32:12 +0800

    comment update

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -29,7 +29,7 @@ const Work = () => {
                                 <div className="pt-8 text-center">
                                    <a href='https://x-clone-6xtz.onrender.com'>
                                       <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                         Live
+                                         Live (It might take some time to load)
                                       </button>
                                    </a>
                                 </div>
